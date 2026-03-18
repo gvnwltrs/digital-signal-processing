6efea10552b8de9affff0d4edd2207ecc12522b7
Gavin Walters <waltersg298@gmail.com>
2026-03-18 08:22:22 -0600

    FEAT: Added template: RCA architecture.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+/*******************************************************************************
+ * Rust Regulated Cell Architecture (RCA) 
+ * Author: Gavin Walters
+ * Created: 2026-03-06
+ * 
+ * Description: 
+ * Linear Sequential Runtime System 
+ * 
+ * 
+ * Workflow:
+ * Data -> Constraints -> Cells -> Threads -> Engine
+ * 
+******************************************************************************/
+
+// Data
+#[allow(unused)]
+use std::marker::PhantomData;
+use std::io::Error;
+#[allow(unused)]
+use std::time::Instant;
+
+// Modules
+#[allow(unused)]
+use digital_signal_processing::rca::*;
+
+/*******************************************************************************
+ * Runtime Engine 
+******************************************************************************/
+
+fn main() -> Result<(), Error> {
+    let mut engine = <Engine as PrimaryRunner>::give_default();
+    engine.try_run_engine()?;
+    Ok(())
 }
